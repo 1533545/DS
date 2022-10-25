d2517c2a5b9a11af0896849a6a4474b967915395
huihongwu <80068776+huihongwu@users.noreply.github.com>
2022-10-25 19:40:34 +0200

    Update testProject.java

--- src/test/testProject.java
+++ src/test/testProject.java
@@ -2,4 +2,28 @@ package test;
 
 public class testProject {
     //TODO: DO SOMETHING
+/*
+    //task transportation
+    transportation.startTask();
+    Thread.sleep(4000);
+    transportation.finishTask();
+    Thread.sleep(2000);
+
+    //task first list
+    firstList.startTask();
+    Thread.sleep(6000);
+
+    //task second list
+    secondList.startTask();
+    Thread.sleep(4000);
+
+    firstList.finishTask();
+    Thread.sleep(2000);
+    secondList.finishTask();
+    Thread.sleep(2000);
+
+    transportation.startTask();
+    Thread.sleep(4000);
+    transportation.finishTask();
+*/
 }
