d31892cf050fe14a10e16569c947dcf106358dfe
1565656 <114741352+1565656@users.noreply.github.com>
2022-10-27 17:37:25 +0200

    Update ClockTest.java

--- src/test/java/Kernel/ClockTest.java
+++ src/test/java/Kernel/ClockTest.java
@@ -1,7 +1,19 @@
 package Kernel;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.Test;
+
 
 class ClockTest {
-
+  @Test
+  void clockTesting(){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    Clock c=new Clock();
+    Interval i=new Interval();
+    c.startTimer();
+    c.addObserver(i);
+    c.updateTask();
+    i.update(c,i.getEnd());
+    System.out.println("The time is: "+(i.getEnd()).format(formatter));
+  }
 }
