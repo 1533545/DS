47ab657162012c651ff9f80241315faba996bb63
1565656 <114741352+1565656@users.noreply.github.com>
2022-10-27 17:35:41 +0200

    Update Clock.java

--- src/main/java/Kernel/Clock.java
+++ src/main/java/Kernel/Clock.java
@@ -3,17 +3,30 @@ package Kernel;
 import java.util.Timer;
 import java.time.LocalDateTime;
 import java.util.Observable;
+import java.util.TimerTask;
 
 public class Clock extends Observable {
-    private Timer _timer;
+    private Timer _timer=null;
 
     public void startTimer() {
-        this._timer = new Timer();
+        if(this._timer==null)
+        {
+            this._timer = new Timer();
+        }
     }
+    public void updateTask()
+    {
+        this._timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                tick();
+            }
+        },0,3000);
 
+    }
     private void tick() {
         LocalDateTime dateTime = LocalDateTime.now();
         setChanged();
-        notifyObservers(this);
+        notifyObservers(dateTime);
     }
 }
