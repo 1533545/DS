c962fca4bbad5eb94c8d618262e3b6695a5264b8
1565656 <114741352+1565656@users.noreply.github.com>
2022-11-03 20:41:35 +0100

    Update Interval.java

--- src/main/java/Kernel/Interval.java
+++ src/main/java/Kernel/Interval.java
@@ -1,4 +1,4 @@
-package Kernel;//package Kernel;
+package Kernel;
 
 import org.json.JSONObject;
 
@@ -9,8 +9,16 @@ import java.util.Observer;
 import java.util.Observable;
 
 public class Interval implements Observer{ //Implements Observer
+  public Task fatherTask;
   private LocalDateTime _start;
   private LocalDateTime _end;
+
+
+  public Interval(Task task) {
+    this._start = LocalDateTime.now();
+    this._end = LocalDateTime.now();
+    this.fatherTask = task;
+  }
 
   public Interval() {
     this._start = LocalDateTime.now();
@@ -41,10 +49,38 @@ public class Interval implements Observer{ //Implements Observer
   public Duration getDuration() {
     return Duration.between(_start,_end);
   }
+  /*public void showTaskInfo()
+  {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    System.out.println("Interval: Initial date: "+this._start.format(formatter)+", Final date: "
+        +this._end.format(formatter)+", Duration: "+this.getDuration());
+
+  }*/
+
+  public void showProjectInfo()
+  {
+//    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//    System.out.println("Task: Initial date: "+this._start.format(formatter)+", Final date: "
+//        +this._end.format(formatter)+", Duration: "+this.getDuration());
+  }
 
   @Override
   public void update(Observable obs,Object arg) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     this._end=(LocalDateTime) arg;
+    this.fatherTask.updateTime(getDuration());
+
+    System.out.println("Interval: Initial date: "+ this._start.format(formatter)+", Final date: "
+        +this._end.format(formatter)+", Duration: "+ this.getDuration());
+    System.out.println("Task: "+ this.fatherTask.Name + " Initial date: "+this._start.format(formatter)+", Final date: "
+        +this._end.format(formatter)+", Duration: "+ this.fatherTask.CompletedWork.plus(this.getDuration()));
+
+    ProjectComponent projectNode = this.fatherTask._fatherNode;
+    while(projectNode != null) {
+      System.out.println("Project: " + this.fatherTask._fatherNode.Name + " Initial date: "+this._start.format(formatter)+", Final date: "
+          + this._end.format(formatter) + ", Duration: "+ projectNode.CompletedWork.plus(this.getDuration()) + "\n");
+      projectNode = projectNode._fatherNode;
+    }
   }
 
   @Override
