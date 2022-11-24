ef9dba42ec42f16a259de2d966e20b0ff7bd0f24
1565656 <114741352+1565656@users.noreply.github.com>
2022-11-24 18:42:47 +0100

    Update Interval.java

--- src/main/java/Kernel/Interval.java
+++ src/main/java/Kernel/Interval.java
@@ -1,66 +1,84 @@
-package Kernel;
+package kernel;
 
-import org.json.JSONObject;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Observer;
 import java.util.Observable;
+import java.util.Observer;
+import java.util.UUID;
+import org.json.JSONObject;
 
-/*
-* Observer class. Contains start and finish times of a task time interval.
-* Generates intervals of time every time a task starts. Keeps them and can
-* return the duration of the interval.
-*/
+
+
+/**
+ * Observer class. Contains start and finish times of a task time interval.
+ * Generates intervals of time every time a task starts. Keeps them and can
+ * return the duration of the interval.
+ **/
 
 public class Interval implements Observer {
-  private Task _fatherTask;
-  private LocalDateTime _start;
-  private LocalDateTime _end;
+  private Task fatherTask;
+  private LocalDateTime start;
+  private LocalDateTime end;
+  private String id;
 
+  /**
+   * comment.
+   **/
   public Interval(Task task) {
-    this._start = LocalDateTime.now();
-    this._fatherTask = task;
-    this._fatherTask.updateStartTime(this._start);
+    this.start = LocalDateTime.now();
+    this.fatherTask = task;
+    this.fatherTask.updateStartTime(this.start);
+    id = generateUuid();
   }
 
+  /**
+   * comment.
+   **/
   public Interval(JSONObject jsonObject) {
     DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-    this._start = LocalDateTime.parse(jsonObject.get("Start").toString(),formatter);
-    this._end = LocalDateTime.parse(jsonObject.get("End").toString(),formatter);
+    this.start = LocalDateTime.parse(jsonObject.get("Start").toString(), formatter);
+    this.end = LocalDateTime.parse(jsonObject.get("End").toString(), formatter);
+    this.id = jsonObject.get("Id").toString();
   }
 
+  /**
+   * comment.
+   **/
   public JSONObject toJson() {
     JSONObject jsonObject = new JSONObject();
-    jsonObject.put("Start", this._start);
-    jsonObject.put("End", this._end);
+    jsonObject.put("Start", this.start);
+    jsonObject.put("End", this.end);
+    jsonObject.put("Id", this.id);
     return jsonObject;
   }
 
   public Duration getDuration() {
-    return Duration.between(_start,_end);
+    return Duration.between(start, end);
   }
 
   @Override
-  public void update(Observable obs,Object arg) {
+  public void update(Observable obs, Object arg) {
     LocalDateTime time = (LocalDateTime) arg;
-    this._end = time;
-    this._fatherTask.updateFinishTime(time);
+    this.end = time;
+    this.fatherTask.updateFinishTime(time);
     printTimes();
   }
 
   private void printTimes() {
     printIntervalTimes();
-    this._fatherTask.printTimes();
-    this._fatherTask.printComponentTimes();
+    this.fatherTask.printTimes();
+    this.fatherTask.printComponentTimes();
   }
 
   private void printIntervalTimes() {
     System.out.println("-------------------------------------------------");
-    System.out.println("Interval -> Start: " + this._start.format(DateTimeFormatter.ISO_DATE_TIME));
-    System.out.println("            End: " + this._end.format(DateTimeFormatter.ISO_DATE_TIME));
+    System.out.println("Interval -> Start: " + this.start.format(DateTimeFormatter.ISO_DATE_TIME));
+    System.out.println("            End: " + this.end.format(DateTimeFormatter.ISO_DATE_TIME));
     System.out.println("            Duration: " + getDuration());
   }
 
-
+  protected String generateUuid() {
+    return UUID.randomUUID().toString();
+  }
 }
