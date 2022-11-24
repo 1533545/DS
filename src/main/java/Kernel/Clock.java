1235d91014c46a4046b2502c8a551356137f130a
1565656 <114741352+1565656@users.noreply.github.com>
2022-11-24 18:42:13 +0100

    Update Clock.java

--- src/main/java/Kernel/Clock.java
+++ src/main/java/Kernel/Clock.java
@@ -1,54 +1,62 @@
-package Kernel;
+package kernel;
 
-import java.util.Timer;
 import java.time.LocalDateTime;
 import java.util.Observable;
+import java.util.Timer;
 import java.util.TimerTask;
 
-/*
-* Observable class. Is in charge of returning the real time
-* every two seconds to all of his observers.
-*/
+/**
+ * Observable class. Is in charge of returning the real time
+ * every two seconds to all of his observers.
+ **/
 public class Clock extends Observable {
-    private Timer _timer;
-    private static Clock _clock;
-    private boolean _cancelTimer;
+  private Timer timer;
+  private static Clock clock;
+  private boolean cancelTimer;
 
-    private Clock() {
-        this._timer = new Timer();
-        this._cancelTimer = false;
-    }
+  private Clock() {
+    this.timer = new Timer();
+    this.cancelTimer = false;
+  }
 
-    /* The synchronized is used in order to manage more than one thread */
-    public synchronized static Clock getInstance() {
-        if(_clock == null)
-        {
-            _clock = new Clock();
-        }
-        return _clock;
+  /**
+   *  The synchronized is used in order to manage more than one thread.
+   **/
+  public static synchronized Clock getInstance() {
+    if (clock == null) {
+      clock = new Clock();
     }
+    return clock;
+  }
 
-    public void startClock() {
-        this._timer.schedule(new TimerTask() { @Override public void run() { tick(); } },2000,2000);
-    }
+  /**
+   * comment.
+   **/
+  public void startClock() {
+    this.timer.schedule(new TimerTask() {
+      @Override
+      public void run() {
+        tick();
+      }
+    }, 2000, 2000);
+  }
 
-    private void tick() {
-        if(!_cancelTimer) {
-            setChanged();
-            notifyObservers(LocalDateTime.now());
-        }
-        else {
-            stopTimer();
-        }
+  private void tick() {
+    if (!cancelTimer) {
+      setChanged();
+      notifyObservers(LocalDateTime.now());
+    } else {
+      stopTimer();
     }
+  }
 
-    private void stopTimer() {
-        this._timer.cancel();
-        this._timer.purge();
-        this._timer=null;
-    }
+  private void stopTimer() {
+    this.timer.cancel();
+    this.timer.purge();
+    this.timer = null;
+  }
 
-    public void stopClock() {
-        this._cancelTimer = true;
-    }
+  public void stopClock() {
+    this.cancelTimer = true;
+  }
 }
