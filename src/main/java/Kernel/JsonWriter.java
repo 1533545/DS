80ecdcd57f5ccb1d1e4e5ecbd17aa4c833013b9c
1565656 <114741352+1565656@users.noreply.github.com>
2022-11-24 18:43:30 +0100

    Update JsonWriter.java

--- src/main/java/Kernel/JsonWriter.java
+++ src/main/java/Kernel/JsonWriter.java
@@ -1,29 +1,37 @@
 //Class JsonWriter, class created to save the tree build in projectComponent in a .txt file
 
-package Kernel;
+package json;
 
-import org.json.JSONObject;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import org.json.JSONObject;
 
-/*
+/**
  * Writes a new JsonString in a new file.
  * Can generate 2 new files:
  *      - json: .txt file of only 1 line with the json object write as string.
  *      - jsonPrettier: Readable .txt file with the json object passed.
- */
+ **/
 public class JsonWriter {
+
+  /**
+   * comment.
+   */
   public static void saveJson(JSONObject json) {
     try {
-      writeJson(json.toString(),"Json");
+      writeJson(json.toString(), "json");
     } catch (IOException e) {
       System.out.println(e);
     }
   }
 
+  /**
+   * comment.
+   */
   public static void saveJsonPrettier(JSONObject json) {
     try {
-      writeJson(json.toString(2),"JsonPrettier");
+      writeJson(json.toString(2), "JsonPrettier");
     } catch (IOException e) {
       System.out.println(e);
     }
