dd227b2f4d6fa40d844aeb077e54b1c7aa0df6e6
1565656 <114741352+1565656@users.noreply.github.com>
2022-11-24 18:44:08 +0100

    Update Project.java

--- src/main/java/Kernel/Project.java
+++ src/main/java/Kernel/Project.java
@@ -1,128 +1,145 @@
-package Kernel;
+package kernel;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.time.Duration;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import visitor.Visitor;
 
-/*
+/**
  * Extension of Component. Container of Components, in other words,
  * contains multiple Projects or/and Tasks. Represents a project node of the components tree.
- */
+ **/
 public class Project extends Component {
-    private List<Component> _children;
+  private List<Component> children;
 
-    public Project(Component fatherNode, String name, String description) {
-        super(fatherNode, name, description);
-        this._children = new ArrayList<>();
+  /**
+   * comment.
+   **/
+  public Project(Component fatherNode, String name, String description) {
+    super(fatherNode, name, description);
+    if ((name.replaceAll("\\s", "")).matches("^[a-zA-Z0-9]*$") && name != null) {
+      this.children = new ArrayList<>();
+    } else {
+      throw new IllegalArgumentException("Illegal project name:" + name);
     }
 
-    public Project(JSONObject jsonObject) throws Exception {
-        super(jsonObject);
-        JSONArray jsonArray  = jsonObject.getJSONArray("Children");
-        this._children = new ArrayList<>();
-        generateChildrenFromJson(jsonArray);
+
+    invariant();
+  }
+
+  /**
+   * comment.
+   */
+  public Project(JSONObject jsonObject) throws Exception {
+    super(jsonObject);
+    if (jsonObject != null) {
+      JSONArray jsonArray = jsonObject.getJSONArray("Children");
+      this.children = new ArrayList<>();
+      generateChildrenFromJson(jsonArray);
+    } else {
+      throw new Exception("No file to read");
     }
 
-    @Override
-    public void acceptVisitor(Visitor visitor) {
-        visitor.visitProject(this);
-    }
 
-    private void generateChildrenFromJson(JSONArray jsonArray) throws Exception {
-        for(int i = 0; i < jsonArray.length(); i++) {
-            JSONObject childJson = jsonArray.getJSONObject(i);
-            String objectType = childJson.get("Class").toString();
+    invariant();
+  }
 
-            if(objectType.equals("Task")) {
-                generateTaskFromJson(childJson);
-            }
-            else if(objectType.equals("Project")){
-                generateProjectFromJson(childJson);
-            }
-            else {
-                throw new Exception("No class match");
-            }
-        }
-    }
+  @Override
+  public void acceptVisitor(Visitor visitor) {
+    visitor.visitProject(this);
+  }
 
-    private void generateTaskFromJson(JSONObject childJson) {
-        Task task = new Task(childJson);
-        task.fatherNode = this;
-        this._children.add(task);
-    }
+  private void generateChildrenFromJson(JSONArray jsonArray) throws Exception {
+    for (int i = 0; i < jsonArray.length(); i++) {
+      JSONObject childJson = jsonArray.getJSONObject(i);
+      String objectType = childJson.get("Class").toString();
 
-    private void generateProjectFromJson(JSONObject childJson) throws Exception {
-        Project task = new Project(childJson);
-        task.fatherNode = this;
-        this._children.add(task);
+      if (objectType.equals("Task")) {
+        generateTaskFromJson(childJson);
+      } else if (objectType.equals("Project")) {
+        generateProjectFromJson(childJson);
+      } else {
+        throw new Exception("No class match");
+      }
     }
+  }
 
-    @Override
-    public JSONObject toJson() {
-        JSONObject jsonObject = toJsonComponent(new JSONObject());
-        jsonObject.put("Class", "Project");
-        jsonObject.put("Children", childrenToJson());
-        return jsonObject;
-    }
+  private void generateTaskFromJson(JSONObject childJson) throws Exception {
+    Task task = new Task(childJson);
+    task.fatherNode = this;
+    this.children.add(task);
+  }
 
-    private JSONArray childrenToJson() {
-        JSONArray jsonArray = new JSONArray();
-        for (Component child : _children){
-            jsonArray.put(child.toJson());
-        }
-        return jsonArray;
-    }
+  private void generateProjectFromJson(JSONObject childJson) throws Exception {
+    Project task = new Project(childJson);
+    task.fatherNode = this;
+    this.children.add(task);
+  }
 
-    public boolean RemoveComponent(String id) {
-        Component componentById = getComponentById(id);
-        if(componentById == null)
-        {
-            return false;
-        }
-        this._children.remove(componentById);
-        return true;
-    }
+  @Override
+  public JSONObject toJson() {
+    JSONObject jsonObject = toJsonComponent(new JSONObject());
+    jsonObject.put("Class", "Project");
+    jsonObject.put("Children", childrenToJson());
+    assert (jsonObject != null);
+    return jsonObject;
+  }
 
-    private Component getComponentById(String id) {
-        for (Component projectComponent : this._children)
-        {
-            if (Objects.equals(projectComponent.id, id))
-            {
-                return projectComponent;
-            }
-        }
-        return null;
+  private JSONArray childrenToJson() {
+    JSONArray jsonArray = new JSONArray();
+    for (Component child : children) {
+      jsonArray.put(child.toJson());
     }
+    return jsonArray;
+  }
 
-    public void addComponent(Component projectComponent) {
-        this._children.add(projectComponent);
-        projectComponent.fatherNode = this;
+  /**
+   *comment.
+   */
+  public void addComponent(Component projectComponent) {
+    invariant();
+    if (projectComponent != null) {
+      this.children.add(projectComponent);
+      projectComponent.fatherNode = this;
     }
-    public List<Component> getChildren(){
-        return this._children;
-    }
-    @Override
-    public Duration getDuration() {
-        Duration duration = Duration.between(LocalTime.NOON,LocalTime.NOON);
-        for (Component child : this._children) {
-            duration = duration.plus(child.getDuration());
-        }
-        return duration;
-    }
+    invariant();
+  }
 
-    public void print(int indentation) {
-        String customIndentation = generateCustomIndentation(indentation);
-        System.out.println(customIndentation + ">" + "PROJECT: " + this.name + " - Start: " +
-                this.getStartTime() + " - Finish: " + this.getFinishTime() + " - Duration: " + this.getDuration());
-        for (Component child : this._children) {
-            child.print(indentation + 2);
-        }
-    }
+  public List<Component> getChildren() {
+    return this.children;
+  }
 
+  @Override
+  public Duration getDuration() {
+    invariant();
+    Duration duration = Duration.between(LocalTime.NOON, LocalTime.NOON);
+    for (Component child : this.children) {
+      duration = duration.plus(child.getDuration());
+    }
+    assert (duration.isPositive());
+    invariant();
+    return duration;
+  }
+
+  /**
+   * comment.
+   */
+  public void print(int indentation) {
+    String customIndentation = generateCustomIndentation(indentation);
+    System.out.println(customIndentation + ">" + "PROJECT: " + this.name
+        + " - Start: " +  this.getStartTime() + " - Finish: "
+        + this.getFinishTime() + " - Duration: " + this.getDuration());
+    for (Component child : this.children) {
+      child.print(indentation + 2);
+    }
+  }
+
+  private boolean invariant() {
+    assert (this.children != null);
+    return true;
+  }
 }
